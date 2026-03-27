dcb7b1182245481cbbfcdee46c07e2fe8a787eb2
Ulver Pedersen <ulpe@ek.dk>
2026-03-27 13:43:03 +0000

    Ændret fra produkter til vindmøller

--- app.py
+++ app.py
@@ -3,32 +3,32 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 # Simpel in-memory database til demo
-produkter = [
-    {"id": 1, "navn": "Laptop", "pris": 9999},
-    {"id": 2, "navn": "Mus", "pris": 199},
+vindmoeller = [
+    {"id": 1, "navn": "Vestas 1", "MW": 1,8},
+    {"id": 2, "navn": "Vestas 2", "MW": 3},
 ]
 
-@app.route('/api/produkter', methods=['GET'])
-def hent_produkter():
-    return jsonify(produkter)
+@app.route('/api/vindmoeller', methods=['GET'])
+def hent_vindmoeller():
+    return jsonify(vindmoeller)
 
-@app.route('/api/produkter/<int:id>', methods=['GET'])
-def hent_produkt(id):
-    produkt = next((p for p in produkter if p["id"] == id), None)
-    if produkt:
-        return jsonify(produkt)
-    return jsonify({"fejl": "Produkt ikke fundet"}), 404
+@app.route('/api/vindmoeller/<int:id>', methods=['GET'])
+def hent_vindmoeller(id):
+    vindmoelle = next((p for p in vindmoeller if p["id"] == id), None)
+    if vindmoelle:
+        return jsonify(vindmoelle)
+    return jsonify({"fejl": "Vindmølle ikke fundet"}), 404
 
-@app.route('/api/produkter', methods=['POST'])
-def opret_produkt():
+@app.route('/api/vindmoeller', methods=['POST'])
+def opret_vindmoelle():
     data = request.get_json()
-    nyt_produkt = {
-        "id": len(produkter) + 1,
+    ny_vindmoelle = {
+        "id": len(vindmoeller) + 1,
         "navn": data["navn"],
-        "pris": data["pris"]
+        "pris": data["MW"]
     }
-    produkter.append(nyt_produkt)
-    return jsonify(nyt_produkt), 201
+    produkter.append(ny_vindmoelle)
+    return jsonify(ny_vindmoelle), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
